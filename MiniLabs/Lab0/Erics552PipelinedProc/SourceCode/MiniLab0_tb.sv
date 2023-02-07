9e8286658ca37382fc6e93aacb534920b269ee10
Madhav Rathi <mrathi3@wisc.edu>
2023-02-07 15:41:04 -0600

    Self checking tb

--- MiniLabs/Lab0/Erics552PipelinedProc/SourceCode/MiniLab0_tb.sv
+++ MiniLabs/Lab0/Erics552PipelinedProc/SourceCode/MiniLab0_tb.sv
@@ -20,17 +20,26 @@ module MiniLab0_tb();
     	rst_n = 1;
 		// LED should capture the SW value 'h155
 		repeat (10) @(posedge clk);
-		$display("LEDR: %h", LEDR);
+		if (LEDR == SW)
+			$display("LED shows SW value! LEDR: %h", LEDR);
+		else
+			$display("Uhh! LED is faulty :)");
 		repeat (10) @(posedge clk);
 		SW = 10'h234;
 		// LED should capture the SW value 'h234
 		repeat (10) @(posedge clk);
-		$display("LEDR: %h", LEDR);
+		if (LEDR == SW)
+			$display("LED shows SW value! LEDR: %h", LEDR);
+		else
+			$display("Uhh! LED is faulty :)");
 		repeat (10) @(posedge clk);
 		SW = 10'h377;
 		// LED should capture the SW value 'h377
 		repeat (10) @(posedge clk);
-		$display("LEDR: %h", LEDR);
+		if (LEDR == SW)
+			$display("LED shows SW value! LEDR: %h", LEDR);
+		else
+			$display("Uhh! LED is faulty :)");
 	end
 
 	always
