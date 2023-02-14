3d12cb4c4de24f789c0d877dad4924729fa181e8
acfranz2 <acfranz2@gmail.com>
2023-02-14 17:59:54 -0600

    implement queue and queue_tb

---
+++ MiniLabs/Lab1/queue.sv
@@ -0,0 +1,37 @@
+module queue(clk, rst_n, in_data, re, we, out_data, free_entries, empty, full);
+	input clk, rst_n;
+	input [7:0] in_data;
+	input re, we;
+	
+	output [7:0] out_data;
+	output [3:0] free_entries;
+	output empty, full;
+	
+	logic [7:0] buffer [7:0];
+	logic [3:0] new_ptr, old_ptr;
+
+	// continuous assign for out_data from wherever old_ptr is pointing
+	assign out_data = buffer[old_ptr[2:0]];
+	assign empty = old_ptr == new_ptr;
+	assign full = new_ptr[2:0] == old_ptr[2:0] && old_ptr[3] != new_ptr[3];
+	assign free_entries = new_ptr[3] == old_ptr[3] ? 8 - (new_ptr[2:0] - old_ptr[2:0]) : old_ptr[2:0] - new_ptr[2:0];
+
+	// logic for maintaining old_ptr
+	always_ff @(posedge clk, negedge rst_n)
+		if (~rst_n) // reset
+			old_ptr <= 4'b000;
+		else if (re & !empty) // increment old_ptr only if there is data to be read and read is enabled
+			old_ptr <= old_ptr + 4'b001;
+		
+    // logic for maintaining new_ptr		
+	always_ff @(posedge clk, negedge rst_n)
+		if (~rst_n)
+			new_ptr <= 4'b0000;
+		else if (we && ~full) // increment only if write is enable and queue not full
+			new_ptr <= new_ptr + 4'b0001;
+
+	always_ff @(posedge clk)
+		if (we && ~full)
+			buffer[new_ptr[2:0]] <= in_data;
+	
+endmodule
---
+++ MiniLabs/Lab1/queue_tb.sv
@@ -0,0 +1,78 @@
+module queue_tb();
+	logic clk, rst_n;
+	logic [7:0] in_data, out_data;
+	logic re, we;
+	logic empty, full;
+	
+	queue iDUT(.clk(clk), .rst_n(rst_n), .in_data(in_data), .re(re), .we(we), .out_data(out_data), .free_entries(), .empty(), .full());
+
+	initial begin
+		clk = 0;
+		rst_n = 0;
+		re = 0;
+		we = 0;
+		in_data = 8'h00;
+		
+		@(posedge clk);
+		@(negedge clk);
+		rst_n = 1; // deassert reset
+		
+		write(clk, in_data, we, 8'h00);
+		
+		write(clk, in_data, we, 8'h01);
+		
+		write(clk, in_data, we, 8'h02);
+		
+		write(clk, in_data, we, 8'h03);
+		
+		write(clk, in_data, we, 8'h04);
+		
+		write(clk, in_data, we, 8'h05);
+		
+		write(clk, in_data, we, 8'h06);
+		
+		write(clk, in_data, we, 8'h07);
+		
+		write(clk, in_data, we, 8'h08);
+		
+		read(clk, re);
+		
+		write(clk, in_data, we, 8'h08);
+		
+		read(clk, re);
+		read(clk, re);
+		read(clk, re);
+		read(clk, re);
+		read(clk, re);
+		read(clk, re);
+		read(clk, re);
+		read(clk, re);
+		read(clk, re);
+		read(clk, re);
+		
+		@(posedge clk);
+		$stop;
+	end
+
+	always
+		#5 clk = ~clk;
+		
+	task automatic write(ref clk, ref [7:0] in_data, ref we, input [7:0] write_data);
+		begin
+			@(posedge clk);
+			in_data = write_data;
+			we = 1;
+			@(posedge clk);
+			we = 0;
+		end
+	endtask
+	
+	task automatic read(ref clk, ref re);
+		begin
+			@(posedge clk);
+			re = 1;
+			@(posedge clk);
+			re = 0;
+		end
+	endtask
+endmodule
